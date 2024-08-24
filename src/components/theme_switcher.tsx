eef3af719898aeaa261ab29c03ff9766130e1e92
Matjaž Čelan <matjazcelangithub@gmail.com>
2024-08-24 19:00:13 +0200

    update theme switcher

--- src/components/theme_switcher.tsx
+++ src/components/theme_switcher.tsx
@@ -11,16 +11,11 @@ export const ThemeSwitcher = () => {
     const [isDarkTheme, setIsDarkTheme] = React.useState(localStorage.getItem('theme') === 'dark');
 
     return <nav className="theme-switcher">
-        <span className={isDarkTheme ? '' : 'hidden'} onClick={(e) => {
-            setIsDarkTheme(false);
-            setDarkTheme(false);
+        <span  onClick={(e) => {
+            setIsDarkTheme(isDark => !isDark);
+            setDarkTheme(!isDarkTheme);
             e.stopPropagation();
-        }}><SunIcon /></span>
-        <span className={isDarkTheme ? 'hidden' : ''} onClick={(e) => {
-            setIsDarkTheme(true);
-            setDarkTheme(true);
-            e.stopPropagation();
-        }}><MoonIcon /></span>
+        }}>{isDarkTheme ? <SunIcon /> : <MoonIcon />}</span>
     </nav>
 }
 
